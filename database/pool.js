dc09dd67e14ddf0cbd0dfc7418a02c781ea12b3f
daniyal-abbassi <slimshady3753@protonmail.com>
2025-03-29 02:53:55 +0330

    feat: configure database connection

---
+++ database/pool.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+//handle erros while connecting to database
+
+pool.on('error',(error,client)=>{
+    console.error(`Unexpected error on connection: ${error}`)
+})
+
+pool.connect()
+    .then(()=>console.log("Database connected successfully!!!"))
+    .catch(err => console.error(`Database connection error: ${err}`));
+
+
+module.exports=pool;
