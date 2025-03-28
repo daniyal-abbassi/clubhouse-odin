404aa6b239f73da12378498a9febebd9e41aa41c
daniyal-abbassi <slimshady3753@protonmail.com>
2025-03-29 01:45:55 +0330

    Setup express-ejs-layout logic

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+//require layout
+const expressLayouts = require('express-ejs-layouts');
 
 
+//use public folder
+app.use(express.static('public'));
+//use expressLayouts
+app.use(expressLayouts);
+//set layout
+app.set('layout','./layouts/main');
+//set view engine
+app.set('view engine','ejs');
 
 
 
